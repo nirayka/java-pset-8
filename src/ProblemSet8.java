817b0094624e25e344f556af487e2e296306858d
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2021-01-24 17:50:46 -0500

    first two problems woooo

--- src/ProblemSet8.java
+++ src/ProblemSet8.java
@@ -1,11 +1,24 @@
 public class ProblemSet8 {
 
     public boolean common(int[] a, int[] b) {
-
+        if (a == null || b == null || a.length == 0 || b.length == 0) {
+            return false;
+        } else {
+            if ((a[a.length-1] == b[b.length-1]) || (a[0] == b[0])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     public int[] middleman(int[] a, int[] b) {
-
+        if (a == null || b == null || a.length == 0 || b.length == 0 || a.length % 2 != 1 || b.length % 2 != 1) {
+            return null;
+        } else {
+            int[] answer = new int[] {a[a.length/2], b[b.length/2]};
+            return answer;
+        }
     }
 
     public int[] bigger(int[] a, int[] b) {
