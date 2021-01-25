bcff8d556b5056779edd4895e9f5840cd0984827
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2021-01-24 19:26:21 -0500

    nearly finished?

--- src/ProblemSet8.java
+++ src/ProblemSet8.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class ProblemSet8 {
 
     public boolean common(int[] a, int[] b) {
@@ -15,10 +13,10 @@ public class ProblemSet8 {
     }
 
     public int[] middleman(int[] a, int[] b) {
-        if (a == null || b == null || a.length == 0 || b.length == 0 || a.length % 2 != 1 || b.length % 2 != 1) {
+        if (a == null || b == null || a.length % 2 != 1 || b.length % 2 != 1) {
             return null;
         } else {
-            int[] newArray = new int[] {a[a.length/2], b[b.length/2]};
+            int[] newArray = {a[a.length/2], b[b.length/2]};
             return newArray;
         }
     }
@@ -83,15 +81,63 @@ public class ProblemSet8 {
     }
 
     public int headHoncho(int[] a) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 0) {
+                count++;
+            }
+        }
 
+        if (a == null || a.length == 0 || count > 0) {
+            return -1;
+        } else {
+            int largest = 0;
+            for (int k = 0; k < a.length; k++) {
+                if (a[k] > largest) {
+                    largest = a[k];
+                }
+            }
+            return largest;
+        }
     }
 
     public boolean tippingPoint(int[] a, int threshold) {
-
+        if (a == null || threshold == null) {
+            return false;
+        } else {
+            int arraySum = 0;
+            for (int i = 0; i < a.length; i++) {
+                arraySum += a[i];
+            }
+            if (arraySum > threshold) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     public String halfway(int[] a) {
+        if (a == null || a.length == 0 || a.length % 2 != 0) {
+            return null;
+        }
 
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int j = 0; j < a.length/2; j++) {
+            leftSum += a[j];
+        }
+
+        for (int k = (a.length/2); k < a.length; k++) {
+            rightSum += a[k];
+        }
+
+        if (leftSum > rightSum) {
+            return "LEFT";
+        } else {
+            return "RIGHT";
+        }
     }
 
     public int[] sequential(int[] a, int[] b) {
